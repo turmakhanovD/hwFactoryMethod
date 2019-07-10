c7a71ef8db74127bacbea8a69ac1b6097ef2e393
Турмахан Динислам Даулетулы <@corp.itstep>
2019-07-10 16:20:25 +0600

    Factory Method HW

--- HwFactoryMethod/Program.cs
+++ HwFactoryMethod/Program.cs
@@ -1,166 +1,67 @@
-﻿abstract class Product
-{ }
+﻿using System;
 
-class ConcreteProductA : Product
-{ }
-
-class ConcreteProductB : Product
-{ }
-
-abstract class Creator
-{
-    public abstract Product FactoryMethod();
-}
-
-class ConcreteCreatorA : Creator
-{
-    public override Product FactoryMethod() { return new ConcreteProductA(); }
-}
-
-class ConcreteCreatorB : Creator
-{
-    public override Product FactoryMethod() { return new ConcreteProductB(); }
-}
-Участники
-Абстрактный класс Product определяет интерфейс класса, объекты которого надо создавать.
-
-Конкретные классы ConcreteProductA и ConcreteProductB представляют реализацию класса Product. Таких классов может быть множество
-
-Абстрактный класс Creator определяет абстрактный фабричный метод FactoryMethod(), который возвращает объект Product.
-
-Конкретные классы ConcreteCreatorA и ConcreteCreatorB - наследники класса Creator, определяющие свою реализацию метода FactoryMethod(). Причем метод FactoryMethod() каждого отдельного класса-создателя возвращает определенный конкретный тип продукта. Для каждого конкретного класса продукта определяется свой конкретный класс создателя.
-
-Таким образом, класс Creator делегирует создание объекта Product своим наследникам. А классы ConcreteCreatorA и ConcreteCreatorB могут самостоятельно выбирать какой конкретный тип продукта им создавать.
-
-Теперь рассмотрим на реальном примере.Допустим, мы создаем программу для сферы строительства. Возможно, вначале мы захотим построить многоэтажный панельный дом.И для этого выбирается соответствующий подрядчик, который возводит каменные дома. Затем нам захочется построить деревянный дом и для этого также надо будет выбрать нужного подрядчика:
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
 class Program
 {
     static void Main(string[] args)
     {
-        Developer dev = new PanelDeveloper("ООО КирпичСтрой");
-        House house2 = dev.Create();
+        Printer dev = new Canon("Canon");
+        Document house2 = dev.Print();
 
-        dev = new WoodDeveloper("Частный застройщик");
-        House house = dev.Create();
+        dev = new HP("HP");
+        Document house = dev.Print();
 
         Console.ReadLine();
     }
 }
 // абстрактный класс строительной компании
-abstract class Developer
+abstract class Printer
 {
     public string Name { get; set; }
 
-    public Developer(string n)
+    public Printer(string n)
     {
         Name = n;
     }
-    // фабричный метод
-    abstract public House Create();
+
+    abstract public Document Print();
 }
 // строит панельные дома
-class PanelDeveloper : Developer
+class Canon : Printer
 {
-    public PanelDeveloper(string n) : base(n)
+    public Canon(string n) : base(n)
     { }
 
-    public override House Create()
+    public override Document Print()
     {
-        return new PanelHouse();
+        return new Perl();
     }
 }
 // строит деревянные дома
-class WoodDeveloper : Developer
+class HP : Printer
 {
-    public WoodDeveloper(string n) : base(n)
+    public HP(string n) : base(n)
     { }
 
-    public override House Create()
+    public override Document Print()
     {
-        return new WoodHouse();
+        return new Offset();
     }
 }
 
-abstract class House
+abstract class Document
 { }
 
-class PanelHouse : House
+class Perl : Document
 {
-    public PanelHouse()
+    public Perl()
     {
-        Console.WriteLine("Панельный дом построен");
+        Console.WriteLine("Документ распечатан на бумаге Perl, Принтером Canon");
     }
 }
-class WoodHouse : House
+class Offset : Document
 {
-    public WoodHouse()
+    public Offset()
     {
-        Console.WriteLine("Деревянный дом построен");
+        Console.WriteLine("Документ распечатан на бумаге Offset, Принтером HP");
     }
 }
