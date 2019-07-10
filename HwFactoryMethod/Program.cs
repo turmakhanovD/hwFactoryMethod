9123de00b5c3302eb065c9fa5941c87868605ecd
Турмахан Динислам Даулетулы <@corp.itstep>
2019-07-10 16:16:05 +0600

    Добавьте файлы проекта.

---
+++ HwFactoryMethod/Program.cs
@@ -0,0 +1,166 @@
+﻿abstract class Product
+{ }
+
+class ConcreteProductA : Product
+{ }
+
+class ConcreteProductB : Product
+{ }
+
+abstract class Creator
+{
+    public abstract Product FactoryMethod();
+}
+
+class ConcreteCreatorA : Creator
+{
+    public override Product FactoryMethod() { return new ConcreteProductA(); }
+}
+
+class ConcreteCreatorB : Creator
+{
+    public override Product FactoryMethod() { return new ConcreteProductB(); }
+}
+Участники
+Абстрактный класс Product определяет интерфейс класса, объекты которого надо создавать.
+
+Конкретные классы ConcreteProductA и ConcreteProductB представляют реализацию класса Product. Таких классов может быть множество
+
+Абстрактный класс Creator определяет абстрактный фабричный метод FactoryMethod(), который возвращает объект Product.
+
+Конкретные классы ConcreteCreatorA и ConcreteCreatorB - наследники класса Creator, определяющие свою реализацию метода FactoryMethod(). Причем метод FactoryMethod() каждого отдельного класса-создателя возвращает определенный конкретный тип продукта. Для каждого конкретного класса продукта определяется свой конкретный класс создателя.
+
+Таким образом, класс Creator делегирует создание объекта Product своим наследникам. А классы ConcreteCreatorA и ConcreteCreatorB могут самостоятельно выбирать какой конкретный тип продукта им создавать.
+
+Теперь рассмотрим на реальном примере.Допустим, мы создаем программу для сферы строительства. Возможно, вначале мы захотим построить многоэтажный панельный дом.И для этого выбирается соответствующий подрядчик, который возводит каменные дома. Затем нам захочется построить деревянный дом и для этого также надо будет выбрать нужного подрядчика:
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+class Program
+{
+    static void Main(string[] args)
+    {
+        Developer dev = new PanelDeveloper("ООО КирпичСтрой");
+        House house2 = dev.Create();
+
+        dev = new WoodDeveloper("Частный застройщик");
+        House house = dev.Create();
+
+        Console.ReadLine();
+    }
+}
+// абстрактный класс строительной компании
+abstract class Developer
+{
+    public string Name { get; set; }
+
+    public Developer(string n)
+    {
+        Name = n;
+    }
+    // фабричный метод
+    abstract public House Create();
+}
+// строит панельные дома
+class PanelDeveloper : Developer
+{
+    public PanelDeveloper(string n) : base(n)
+    { }
+
+    public override House Create()
+    {
+        return new PanelHouse();
+    }
+}
+// строит деревянные дома
+class WoodDeveloper : Developer
+{
+    public WoodDeveloper(string n) : base(n)
+    { }
+
+    public override House Create()
+    {
+        return new WoodHouse();
+    }
+}
+
+abstract class House
+{ }
+
+class PanelHouse : House
+{
+    public PanelHouse()
+    {
+        Console.WriteLine("Панельный дом построен");
+    }
+}
+class WoodHouse : House
+{
+    public WoodHouse()
+    {
+        Console.WriteLine("Деревянный дом построен");
+    }
+}
